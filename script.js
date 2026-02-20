397f1d3edb27ccf63dac9c5c543ad6c74d8ca727
sailexander <github@sailex.de>
2026-02-20 21:47:21 +0100

    Add grid to canvas background.

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const coordsDiv = document.getElementById("coords");
 const tooltip = document.getElementById("tooltip");
 const fileInput = document.getElementById("fileInput");
 
-let width, height;
+let canvasWidth, canvasHeight, gridSize = 100;
 let scale = 1, offsetX = 0, offsetY = 0;
 
 function resize() {
     console.log("rezising...");
-    width = canvas.width = canvas.parentElement.clientWidth;
-    height = canvas.height = canvas.parentElement.clientHeight;
+    canvasWidth = canvas.width = canvas.parentElement.clientWidth;
+    canvasHeight = canvas.height = canvas.parentElement.clientHeight;
     draw();
 }
 window.addEventListener("resize", resize);
@@ -18,37 +18,65 @@ resize();
 
 function worldToScreen(x, y) {
     return {
-        x: width / 2 + (x + offsetX) * scale,
-        y: height / 2 + (y + offsetY) * scale
+        x: canvasWidth / 2 + (x + offsetX) * scale,
+        y: canvasHeight / 2 + (y + offsetY) * scale
     };
 }
+
 function screenToWorld(x, y) {
     return {
-        x: (x - width / 2) / scale - offsetX,
-        y: (y - height / 2) / scale - offsetY
+        x: (x - canvasWidth / 2) / scale - offsetX,
+        y: (y - canvasHeight / 2) / scale - offsetY
     };
 }
 
 function drawAxes() {
     world.strokeStyle = "#000";
     world.lineWidth = 2;
-    
+
     const origin = worldToScreen(0, 0);
-    
+
     // x-axis
     world.beginPath();
     world.moveTo(0, origin.y);
-    world.lineTo(width, origin.y);
+    world.lineTo(canvasWidth, origin.y);
     world.stroke();
-    
+
     // y-axis
     world.beginPath();
     world.moveTo(origin.x, 0);
-    world.lineTo(origin.x, height);
+    world.lineTo(origin.x, canvasHeight);
     world.stroke();
 }
 
+function drawGrid() {
+    world.strokeStyle = '#acacac';
+    world.lineWidth = 1;
+
+    const minX = -(canvasWidth / 2) / scale - offsetX;
+    const maxX = (canvasWidth / 2) / scale - offsetX;
+    const minY = -(canvasHeight / 2) / scale - offsetY;
+    const maxY = (canvasHeight / 2) / scale - offsetY;
+
+    for (let x = Math.floor(minX / gridSize) * gridSize; x <= maxX; x += gridSize) {
+        const position = worldToScreen(x, 0);
+        world.beginPath();
+        world.moveTo(position.x, 0);
+        world.lineTo(position.x, canvasHeight);
+        world.stroke();
+    }
+
+    for (let y = Math.floor(minY / gridSize) * gridSize; y <= maxY; y += gridSize) {
+        const position = worldToScreen(0, y);
+        world.beginPath();
+        world.moveTo(0, position.y);
+        world.lineTo(canvasWidth, position.y);
+        world.stroke();
+    }
+}
+
 function draw() {
-    world.clearRect(0, 0, width, height);
+    world.clearRect(0, 0, canvasWidth, canvasHeight);
+    drawGrid();
     drawAxes();
 }
